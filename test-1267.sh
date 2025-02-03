f5ac1c02d150f25f248d00cf53b1b3fca6ebc3fc
Álvaro Osvaldo <alvaro@osvaldo.dev.br>
2025-02-03 10:40:36 -0300

    test: Improved 'BOM' parameter end-to-end test
    Signed-off-by: Álvaro Osvaldo <alvaro@osvaldo.dev.br>

--- test-1267.sh
+++ test-1267.sh
@@ -2,22 +2,38 @@
 
 set -e
 
-outputFile="$(mktemp)"
+printf "======== Automated Testing ======== \n"
 
-{
+function validate {
+
+  TITLE="$1"
+  COMMAND="$2"
+  BOM="$3"
+
   setterm --bold on
-  printf "Terminal output\n"
+  printf "%-16.16s:" "$TITLE"
   setterm --default
-  in2csv --add-bom ./examples/dummy.xls | od -h -N 3
+
+  content="$(eval "$COMMAND" | od -h -N 3 | head -n 1)"
+  printf " %-28.28s " "$content"
+
+  if printf "%s\n" "$content" | grep "$BOM" > /dev/null ; then
+    setterm --foreground green
+    printf "PASS\n"
+  else
+    setterm --foreground red --blink on
+    printf "FAIL\n"
+  fi
+
+  setterm --default
+
+}
+{
+  validate "As Parameter" "in2csv --add-bom ./examples/dummy.xls" "bbef"
+  validate "As Pipeline" "cat ./examples/dummy.xls | in2csv --add-bom  --format xls -" "bbef"
 }
 printf "\n"
-{
-  setterm --bold on
-  printf "Pipeline Input\n"
-  setterm --default
-  cat ./examples/dummy.xls | in2csv --add-bom  --format xls - | od -h -N 3
-}
-printf "\n"
+printf "======== Manual validation ======== \n"
 {
   setterm --bold on
   printf "Raw Output With Bom\n"
