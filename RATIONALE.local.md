6a9ae13a314f25dea33ce49b72eaccf0201e7b30
Álvaro Osvaldo <alvaro@osvaldo.dev.br>
2025-02-04 16:59:16 -0300

    test: 'BOM' test verify edge cases
    Signed-off-by: Álvaro Osvaldo <alvaro@osvaldo.dev.br>

--- RATIONALE.local.md
+++ RATIONALE.local.md
@@ -1,6 +1,6 @@
 # Implementation
 
-Implemented the feature to optionality add Byte Order Mark (BOM) into output CSV content.
+Implemented the feature to optionality add Byte Order Mark (BOM) into output CSV content in all utilities except `csvpy` and `sql2csv`
 
 # Solution
 
@@ -12,12 +12,55 @@ Implemented the feature to optionality add Byte Order Mark (BOM) into output CSV
 
 # Tests
 
-- There is end-to-end testing attached to this message.
+- There is end-to-end testing attached to this message, only the 'dummy.xls' example was used for testing.
+- The same script was tested with the following Pythons implementations: 
+  - Python 3.8
+- No other tests was made.
 
 # Checklist
 
+
+
 # Considerations
 
+**Python Deprecation**
+
+```
+DEPRECATION: Legacy editable install of csvkit[test]==2.0.1 from file:///home/alvaro/Desktop/Alvaro%20Osvaldo%20Dev/Externals/Customers/CSVKit/Programs/CSVKit%20Implementation/development (setup.py develop) is deprecated. pip 25.1 will enforce this behaviour change. A possible replacement is to add a pyproject.toml or enable --use-pep517, and use setuptools >= 64. If the resulting installation is not behaving as expected, try using --config-settings editable_mode=compat. Please consult the setuptools documentation for more information. Discussion can be found at https://github.com/pypa/pip/issues/11457
+```
+
+
+**Edge Cases**
+
+
+For some utilities there is some bugs for edge cases when not coding with UTF-8. 
+
+All is show in the test script, and i don't have tracked the problem to know if happens
+due the CSVToolkit or due 'agate' library.
+
+For the specific case for CSVStat , for UTF-32LE ( Little Endian ), there is a warning message from 'agate' 
+and 'Contains null values' show as 'True' and 'Most common values' show the 'None', both results should not exist. 
+
+The failure can be tested with the code below:
+
+```bash
+$ PYTHONIOENCODING=UTF-32LE in2csv --add-bom ./examples/dummy.xls | PYTHONIOENCODING=UTF-32LE csvstat -d , --add-bom -
+# .../site-packages/agate/table/from_csv.py:83: RuntimeWarning: Error sniffing CSV dialect: Could not determine delimiter
+#  kwargs['dialect'] = csv.Sniffer().sniff(sample)
+# ...
+# Contains null values:  True (excluded from calculations)
+# ...
+#        Most common values:    3.0 (1x)
+#                               None (1x)
+
+
+```
+However, all works fine with the same command when is coded with UTF-32BE ( Big Endian ) as shows below: 
+
+```bash
+$ PYTHONIOENCODING=UTF-32BE in2csv --add-bom ./examples/dummy.xls | PYTHONIOENCODING=UTF-32BE csvstat -d , --add-bom -
+# All works fine
+```
 
 # References
 
--- test-1267.sh
+++ test-1267.sh
@@ -2,30 +2,41 @@
 
 set -e
 
-printf "======== Automated Testing ======== \n"
-
 function validate {
 
   TITLE="$1"
   COMMAND="$2"
-  BOM="$3"
 
   declare -A CHECKS
+  declare -A WITHOUT
   declare -a ORDER
 
   CHECKS["UTF8"]="bbef 00"
-  CHECKS["UTF16"]="bbef 00"
+  CHECKS["UTF-16"]="bbef 00"
   CHECKS["UTF-16BE"]="bbef 00"
   CHECKS["UTF-16LE"]="bbef 00"
   CHECKS["UTF32"]="bbef 00"
   CHECKS["UTF-32BE"]="bbef 00"
   CHECKS["UTF-32LE"]="bbef 00"
-  CHECKS["Without"]="2c61 0062"
+  CHECKS["Without"]=""
+
+  WITHOUT["Parameter"]="2c61 0062"
+  WITHOUT["Pipeline"]="2c61 0062"
+  WITHOUT["CSVClean"]="2c61 0062"
+  WITHOUT["CSVCut"]="0a61 0031"
+  WITHOUT["CSVFormat"]="7c61 0062"
+  WITHOUT["CSVGrep"]="2c61 0062"
+  WITHOUT["CSVJoin"]="2c61 0062"
+  WITHOUT["CSVJson"]="7b5b 0022"
+  WITHOUT["CSVLook"]="207c 0061"
+  WITHOUT["CSVSort"]="2c61 0062"
+  WITHOUT["CSVSQL"]="5243 0045"
+  WITHOUT["CSVStack"]="2c61 0062"
+  WITHOUT["CSVStat"]="2020 0031"
 
   ORDER=(
-  "UTF8" "UTF16" "UTF-16BE" "UTF-16LE" "UTF32" "UTF-32BE" "UTF-32LE" "Without"
+  "UTF8" "UTF-16" "UTF-16BE" "UTF-16LE" "UTF32" "UTF-32BE" "UTF-32LE" "Without"
   )
-
 
 
   setterm --bold on
@@ -35,18 +46,19 @@ function validate {
 
   for check in ${ORDER[*]} ; do
 
+    printf " · %-12.12s : " "$check"
+
     toExecute="eval "$COMMAND" | od -h -N 3 | head -n 1 | grep -o --perl-regexp '0000000 \K.*'"
     expectedResult="${CHECKS[$check]}"
     encoding="$check"
 
     if [ "$check" == "Without" ]; then
+
       toExecute="$(printf "%s" "$toExecute" | sed 's/--add-bom//g')"
-    fi
-
-    if [ "$encoding" == "Without" ]; then
+      expectedResult="${WITHOUT[$TITLE]}"
       encoding=""
-    fi
 
+    fi
 
     toExecute="$(printf "%s" "$toExecute" | sed "s/{ENCODING}/$encoding/g")"
 
@@ -54,8 +66,8 @@ function validate {
 
 
     if [[  "$FOUND" =~ "$expectedResult" ]]; then valid=1; else valid=0; fi
+    if [[  -z "$expectedResult" ]]; then valid=0; fi
 
-    printf " · %-12.12s : " "$check"
 
     printf " %-10.10s " "$FOUND"
 
@@ -76,9 +88,10 @@ function validate {
 
 }
 
+function AUTOMATED()
 {
-  validate "As Parameter" "PYTHONIOENCODING={ENCODING} in2csv --add-bom ./examples/dummy.xls"
-  validate "As Pipeline" "cat ./examples/dummy.xls | PYTHONIOENCODING={ENCODING} in2csv --add-bom  --format xls -"
+  validate "Parameter" "PYTHONIOENCODING={ENCODING} in2csv --add-bom ./examples/dummy.xls"
+  validate "Pipeline" "cat ./examples/dummy.xls | PYTHONIOENCODING={ENCODING} in2csv --add-bom  --format xls -"
 
   validate "CSVClean" "PYTHONIOENCODING={ENCODING} in2csv --add-bom ./examples/dummy.xls | PYTHONIOENCODING={ENCODING} csvclean --add-bom --enable-all-checks -"
   validate "CSVCut" "PYTHONIOENCODING={ENCODING} in2csv --add-bom ./examples/dummy.xls | PYTHONIOENCODING={ENCODING} csvcut --add-bom --columns a"
@@ -86,36 +99,75 @@ function validate {
   validate "CSVGrep" "PYTHONIOENCODING={ENCODING} in2csv --add-bom ./examples/dummy.xls | PYTHONIOENCODING={ENCODING} csvgrep --add-bom --column a -m 1.0 -"
   validate "CSVJoin" "PYTHONIOENCODING={ENCODING} in2csv --add-bom ./examples/dummy.xls | PYTHONIOENCODING={ENCODING} csvjoin --add-bom "
   validate "CSVJson" "PYTHONIOENCODING={ENCODING} in2csv --add-bom ./examples/dummy.xls | PYTHONIOENCODING={ENCODING} csvjson --add-bom -"
-  validate "CSVlook" "PYTHONIOENCODING={ENCODING} in2csv --add-bom ./examples/dummy.xls | PYTHONIOENCODING={ENCODING} csvlook --add-bom -"
+  validate "CSVLook" "PYTHONIOENCODING={ENCODING} in2csv --add-bom ./examples/dummy.xls | PYTHONIOENCODING={ENCODING} csvlook --add-bom -"
 
   validate "CSVSort" "PYTHONIOENCODING={ENCODING} in2csv --add-bom ./examples/dummy.xls | PYTHONIOENCODING={ENCODING} csvsort --add-bom -"
   validate "CSVSQL" "PYTHONIOENCODING={ENCODING} in2csv --add-bom ./examples/dummy.xls | PYTHONIOENCODING={ENCODING} csvsql --add-bom"
   validate "CSVStack" "PYTHONIOENCODING={ENCODING} in2csv --add-bom ./examples/dummy.xls | PYTHONIOENCODING={ENCODING} csvstack --add-bom -n NEWCOL -"
   validate "CSVStat" "PYTHONIOENCODING={ENCODING} in2csv --add-bom ./examples/dummy.xls | PYTHONIOENCODING={ENCODING} csvstat -d , --add-bom -"
 
-  # validate "CSVPy" "in2csv --add-bom ./examples/dummy.xls | csvformat"
-  # validate "SQL2csv" "in2csv --add-bom ./examples/dummy.xls | csvformat"
+  # validate "CSVPy" "in2csv --add-bom ./examples/dummy.xls | csvpy"
+  # validate "SQL2csv" "in2csv --add-bom ./examples/dummy.xls | sql2csv"
 
 }
+
+function MANUAL() {
+  CODINGS=(
+  "UTF8" "UTF-16" "UTF-16BE" "UTF-16LE" "UTF32" "UTF-32BE" "UTF-32LE" "Without"
+  )
+
+  for coding in ${CODINGS[*]}; do
+
+    if [[ $coding != "Without" ]]; then ENCODING="$coding" ; else ENCODING=""; fi
+
+    printf "\n"
+    printf " ---------------- %-12.12s ---------------- \n" "$coding"
+
+
+    printf " · With BOM\n"
+    PYTHONIOENCODING=$ENCODING in2csv --add-bom ./examples/dummy.xls
+
+    printf "\n"
+
+    printf " · Without BOM\n"
+    PYTHONIOENCODING=$ENCODING in2csv ./examples/dummy.xls
+
+    printf " ---------------------------------------------- \n"
+    printf "\n"
+
+
+
+    continue
+
+    setterm --bold on
+    printf  "%-8.8s Raw Output With Bom\n" "$coding"
+    setterm --default
+    in2csv --add-bom ./examples/dummy.xls
+
+    printf "\n"
+
+    setterm --bold on
+    printf  "%-8.8s Raw Output Without Bom\n" "$coding"
+    setterm --default
+    in2csv ./examples/dummy.xls
+
+
+  done
+
+  }
+
+MAIN() {
+
+
+printf "======== Automated Testing ======== \n"
+AUTOMATED
+printf "=================================== \n"
+
 printf "\n"
 printf "======== Manual validation ======== \n"
-{
-  setterm --bold on
-  printf "Raw Output With Bom\n"
-  setterm --default
-  in2csv --add-bom ./examples/dummy.xls
-}
-printf "\n"
-{
-  setterm --bold on
-  printf "Raw Output Without Bom\n"
-  setterm --default
-  in2csv ./examples/dummy.xls
-}
-
-in2csv ./examples/dummy.xls > test.log
-
-
-
+MANUAL
+printf "=================================== \n"
 
 }
+
+MAIN
