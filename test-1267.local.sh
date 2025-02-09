53592227e15542300f63aa144417cb501bd69309
Álvaro Osvaldo <alvaro@osvaldo.dev.br>
2025-02-09 18:11:29 -0300

    test: Updated test
    Signed-off-by: Álvaro Osvaldo <alvaro@osvaldo.dev.br>

--- test-1267.local.sh
+++ test-1267.local.sh
@@ -42,24 +42,24 @@ function AUTOMATED() {
 
 printf "======= Automated Validation =======\n"
 
-#validate "In2csv"  "in2csv  --add-bom ./examples/dummy.xls"
-#validate "In2csv (UTF-16BE)"  "in2csv -e utf-16  --add-bom ./examples/test_utf16_big.csv"
-#validate "In2csv (UTF-16LE)"  "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv"
+validate "In2csv"  "in2csv  --add-bom ./examples/dummy.xls"
+validate "In2csv (UTF-16BE)"  "in2csv -e utf-16  --add-bom ./examples/test_utf16_big.csv"
+validate "In2csv (UTF-16LE)"  "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv"
 
-#validate "In2csv (Pipeline)"  "in2csv --add-bom ./examples/dummy.xls | in2csv --add-bom --format csv -"
+validate "In2csv (Pipeline)"  "in2csv --add-bom ./examples/dummy.xls | in2csv --add-bom --format csv -"
 
-#validate "CSVClean" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvclean  --add-bom --enable-all-checks -"
+validate "CSVClean" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvclean  --add-bom --enable-all-checks -"
 validate "CSVCut" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvcut  --add-bom --columns a"
-#validate "CSVFormat" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvformat  --add-bom -D '|' -"
-#validate "CSVGrep" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvgrep  --add-bom --column a -m 1.0 -"
-#validate "CSVJoin" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvjoin  --add-bom "
-#validate "CSVJson" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvjson  --add-bom -"
-#validate "CSVLook" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvlook  --add-bom -"
+validate "CSVFormat" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvformat  --add-bom -D '|' -"
+validate "CSVGrep" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvgrep  --add-bom --column a -m 1.0 -"
+validate "CSVJoin" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvjoin  --add-bom "
+validate "CSVJson" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvjson  --add-bom -"
+validate "CSVLook" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvlook  --add-bom -"
 
-#validate "CSVSort" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvsort  --add-bom -"
-#validate "CSVSQL" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvsql  --add-bom"
-#validate "CSVStack" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvstack  --add-bom -n NEWCOL -"
-#validate "CSVStat" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvstat  -d , --add-bom -"
+validate "CSVSort" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvsort  --add-bom -"
+validate "CSVSQL" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvsql  --add-bom"
+validate "CSVStack" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvstack  --add-bom -n NEWCOL -"
+validate "CSVStat" "in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv |  csvstat  -d , --add-bom -"
 
 printf "====================================\n"
 
@@ -70,7 +70,17 @@ function MANUAL() {
 printf "========= Manual Validation =========\n"
 
 in2csv  --add-bom ./examples/dummy.xls
+
+echo ""
+
 in2csv -e utf-16  --add-bom ./examples/test_utf16_big.csv
+
+echo ""
+
+in2csv -e utf-16  ./examples/test_utf16_big.csv
+
+echo ""
+
 in2csv -e utf-16  --add-bom ./examples/test_utf16_little.csv
 
 printf "=====================================\n"
@@ -80,7 +90,7 @@ printf "=====================================\n"
 function MAIN() {
 
   AUTOMATED
-  #MANUAL
+  MANUAL
 
 }
 
